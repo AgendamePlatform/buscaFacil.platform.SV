b0f6a6820befa040d25f38d0851fea653d44fef5
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-09-09 15:01:36 -0600

    Inicio de autenticacion con goole

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async session({ session, token }) {
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
+});
